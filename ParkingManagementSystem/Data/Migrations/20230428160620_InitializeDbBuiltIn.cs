c2ce85e823f2ad32cd271a73860998f16139d1a5
baohng <bao.hng221b@gmail.com>
2023-04-28 23:07:55 +0700

    initialize database

---
+++ ParkingManagementSystem/Data/Migrations/20230428160620_InitializeDbBuiltIn.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingManagementSystem.Data.Migrations
+{
+    public partial class InitializeDbBuiltIn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
