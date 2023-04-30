2ecc1bfbfe7f90c3996796cc686322e2708ec693
baohng <bao.hng221b@gmail.com>
2023-04-30 10:38:35 +0700

    added Reservation site

--- ParkingManagementSystem/Controllers/HomeController.cs
+++ ParkingManagementSystem/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ParkingManagementSystem.Controllers
             return View();
         }
 
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
