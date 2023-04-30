6c3ee467004be5db6edcaa287e038af74a716387
baohng <bao.hng221b@gmail.com>
2023-04-30 12:15:46 +0700

    added Management section and _LayoutManagement

--- ParkingManagementSystem/Controllers/HomeController.cs
+++ ParkingManagementSystem/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ParkingManagementSystem.Controllers
             return View();
         }
 
+        public IActionResult Management()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
