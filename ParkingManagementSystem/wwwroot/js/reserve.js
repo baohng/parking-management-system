c839f225b3d974994cde8097a99acb64cd946264
baohng <bao.hng221b@gmail.com>
2023-05-09 16:18:35 +0700

    added passing parking-space-id to url

--- ParkingManagementSystem/wwwroot/js/reserve.js
+++
@@ -1,8 +0,0 @@
-﻿function changeUrl() {
-    // Get the parking space ID from the clicked div
-    var parkingSpaceId = $(this).attr('parking-space-id');
-
-    // Do something with the parking space ID, like passing it to a URL
-    var url = '/Reservations/Create?parkingSpaceId=' + parkingSpaceId;
-    window.location.href = url;
-}
